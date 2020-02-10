0f969dded64c812980d513aa492b0d8655b5d499
Stedd <tor.andre.paulsen@gmail.com>
2020-02-10 20:20:40 +0100

    Start and stoppable Thread

--- app/src/main/java/com/example/loomoapp/MainActivity.kt
+++ app/src/main/java/com/example/loomoapp/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.loomoapp
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.TextView
+import androidx.core.os.postDelayed
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.loomoapp.viewModel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,8 +23,12 @@ class MainActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.textView)
     }
 
-    val mHandler = Handler()
+//    val mThread  = Thread("CalcThread")
 
+//    val mRunnable2 = ExampleRunnable()
+
+//    val mThread = ExampleThread()
+    val mHandler = Handler()
     var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +36,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         Log.i("asd", "Activity created")
+
+
+//        val mThread2 = Thread(mRunnable2).start()
+//        Log.i("asd", "Thread State Debug2: ${mThread2.}")
+
+//        mThread.start()
+//        mLooper.start()
+//
+//        if (mLooper.isAlive) {
+//            Log.i("asd", "Thread started")
+//        } else {
+//            Log.i("asd", "Thread did not start")
+//        }
+//        Log.i("asd", "Thread State Debug2: ${mLooper.state}")
 
         btnStartService.setOnClickListener {
             startService()
@@ -40,7 +62,7 @@ class MainActivity : AppCompatActivity() {
             .get(MainActivityViewModel::class.java)
 
         viewModel.text.observe(this, Observer {
-            textView.text= it
+            textView.text = it
         })
 
         viewModel.text.value = "Service not started"
@@ -48,31 +70,89 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
-
-    private fun startService(){
+    private fun startService() {
         Log.i("asd", "Service start command")
         index = 0
-        mToastRunnable.run()
+//        Thread(mRunnable).run()
+        Thread(mRunnable).start()
+//        newThread = Thread(mRunnable)
+//        runOnUiThread(mRunnable)
+//        mThread.run()
+
     }
 
-    private fun stopService (){
+    private fun stopService() {
         Log.i("asd", "Service stop command")
-        mHandler.removeCallbacks(mToastRunnable)
+        mHandler.removeCallbacks(mRunnable)
+
+//        mThread.runThreadMainLoop = false
+//        Log.i("asd", "Thread State Debug3: ${mThread.state}")
         viewModel.text.value = "Service stopped"
     }
 
 
-    val mToastRunnable: Runnable = object : Runnable {
+    private val mRunnable: Runnable = object : Runnable {
         override fun run() {
             index++
             Log.i("asd", "Index: $index. Looping on Thread: ${mHandler.looper.thread}")
-            viewModel.text.value = "Index: $index"
+//            viewModel.text.value = "Index: $index"
             mHandler.post(this)
         }
     }
+
+//        private val mLooper: AsyncTask = object : Looper(){
+//        override fun run() {
+//            index++
+//            Log.i("asd", "Index: $index. Looping on Thread: ${mHandler.looper.thread}")
+////            viewModel.text.value = "Index: $index"
+//            mHandler.post(this)
+//        }
+//    }
+
+
+
+//    private val mThread = object : Thread("CalcThread"){
+//
+//        var runThreadMainLoop = false
+//        override fun run() {
+//            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND)
+//            while (runThreadMainLoop){
+////                            index++
+//                Log.i("asd", "Index: $index. Looping on Thread: ${mHandler.looper.thread}")
+////            viewModel.text.value = "Index: $index"
+//                mHandler.postDelayed(this, 2000)
+//            }
+//            Log.i("asd", "Thread State Debug1: ${this.state}")
+//        }
+//    }
+
+
+//    private val mThread = object : Thread(object : Runnable{
+//        override fun run() {
+////            index++
+//            Log.i("asd", "Index: $index. Looping on Thread: ${mHandler.looper.thread}")
+////            viewModel.text.value = "Index: $index"
+//            mHandler.postDelayed(this,1000)
+//        }
+//    }){}
+
+    class ExampleThread:Thread(){
+        override fun run() {
+            for (i in 0..10 ) {
+                sleep(1000)
+                Log.i("asd", "Index: $i. Looping on Thread: ${currentThread()}")
+            }
+            Log.i("asd", "Thread state: ${this.state}")
+        }
+    }
+
+    class ExampleRunnable:Runnable{
+        override fun run() {
+            for (i in 0..10 ) {
+                Thread.sleep(1000)
+                Log.i("asd", "Index: $i. Looping on Thread: ${Thread.currentThread()}")
+            }
+        }
+    }
+
 }
-
-
-
-
