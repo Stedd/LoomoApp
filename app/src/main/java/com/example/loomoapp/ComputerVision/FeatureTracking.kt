5255f4ed07df639c343e432acf5a3383f41eab63
JakobC-137 <32057556+JakobC-137@users.noreply.github.com>
2020-02-24 16:04:04 +0100

    I don't really know what I'm doing at this point. It seems that for making threads it is best practice to use the 'HandlerThread' object (if you want a thread that loops until you manually kill it) and do it in a service instead of an activity (services have more predictable life-cycle behaviour).

---
+++ app/src/main/java/com/example/loomoapp/ComputerVision/FeatureTracking.kt
@@ -0,0 +1,4 @@
+package com.example.loomoapp.ComputerVision
+
+class FeatureTracking {
+}
---
+++ app/src/main/java/com/example/loomoapp/MainCVService.kt
@@ -0,0 +1,12 @@
+package com.example.loomoapp
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MainCVService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
