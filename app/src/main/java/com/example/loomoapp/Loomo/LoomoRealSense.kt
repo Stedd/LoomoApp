9944ef6fbc5a579d633e7dd1b69fa314d8f53c0a
JakobC-137 <32057556+JakobC-137@users.noreply.github.com>
2020-02-24 13:48:03 +0100

    Made a 'LoomoRealSense' class which is basically the same as Fjetland's version

--- app/src/main/java/com/example/loomoapp/Loomo/LoomoRealSense.kt
+++ app/src/main/java/com/example/loomoapp/Loomo/LoomoRealSense.kt
@@ -1,28 +1,103 @@
+/**
+ * A class that is meant to make the Intel RealSense available for the rest of the code
+ */
 package com.example.loomoapp.Loomo
 
-/**
- * A class that is meant to make the Intel RealSense available for the Loomo
- */
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import com.segway.robot.sdk.base.bind.ServiceBinder
+import com.segway.robot.sdk.vision.Vision
+import com.segway.robot.sdk.vision.stream.StreamType
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
-/**
- * "If you need a singleton - a class that only has got one instance - you can declare the
- * class in the usual way, but use the object keyword instead of class"
- * https://kotlinlang.org/docs/tutorials/kotlin-for-py/objects-and-companion-objects.html
- */
-object LoomoRealSense {
-    val TAG = "LoomoRealSense"
+class LoomoRealSense(context: Context) {
+    companion object {
+        val TAG = "LoomoRealSense"
 
-    const val COLOR_WIDTH = 640
-    const val COLOR_HEIGHT = 480
+        const val COLOR_WIDTH = 640
+        const val COLOR_HEIGHT = 480
 
-    const val FISHEYE_WIDTH = 640
-    const val FISHEYE_HEIGHT = 480
+        const val FISHEYE_WIDTH = 640
+        const val FISHEYE_HEIGHT = 480
 
-    const val SMALL_COLOR_WIDTH = 320
-    const val SMALL_COLOR_HEIGHT = 240
+        const val DEPTH_WIDTH = 320
+        const val DEPTH_HEIGHT = 240
+    }
 
-    const val DEPTH_WIDTH = 320
-    const val DEPTH_HEIGHT = 240
 
+    var mVision = Vision.getInstance()
+
+    private var colorIsActive = false
+    private var fishEyeIsActive = false
+    private var depthIsActive = false
+
+    var mImgColor = Bitmap.createBitmap(COLOR_WIDTH, COLOR_HEIGHT, Bitmap.Config.ARGB_8888)
+    var mImgFishEye = Bitmap.createBitmap(FISHEYE_WIDTH, FISHEYE_HEIGHT, Bitmap.Config.ALPHA_8)
+    var mImgDepth = Bitmap.createBitmap(DEPTH_WIDTH, DEPTH_HEIGHT, Bitmap.Config.RGB_565)
+
+    init {
+        mVision.bindService(context.applicationContext, object : ServiceBinder.BindStateListener {
+            override fun onBind() {
+                Log.d(TAG, "Vision onBind")
+            }
+
+            override fun onUnbind(reason: String?) {
+                Log.d(TAG, "Vision onUnbind")
+            }
+        })
+    }
+
+    fun setActiveStateOfCameras(color: Boolean, fishEye: Boolean, depth: Boolean) {
+        colorIsActive = color
+        fishEyeIsActive = fishEye
+        depthIsActive = depth
+        stopActiveCameras()
+        startActiveCameras()
+    }
+
+    private fun startActiveCameras() {
+        if (colorIsActive || fishEyeIsActive || depthIsActive) {
+            GlobalScope.launch {
+                if (colorIsActive) startColorCamera()
+                if (fishEyeIsActive) startFisheyeCamera()
+                if (depthIsActive) startDepthCamera()
+            }
+        }
+    }
+
+    private fun stopActiveCameras() {
+        if (mVision.isBind) {
+            mVision.stopListenFrame(StreamType.COLOR)
+            mVision.stopListenFrame(StreamType.FISH_EYE)
+            mVision.stopListenFrame(StreamType.DEPTH)
+        }
+    }
+
+    private suspend fun startColorCamera() {
+        while (!mVision.isBind) {
+            delay(10L)
+        }
+        mVision.startListenFrame(StreamType.COLOR
+        ) { streamType, frame -> mImgColor.copyPixelsFromBuffer(frame.byteBuffer) }
+    }
+
+    private suspend fun startFisheyeCamera() {
+        while (!mVision.isBind) {
+            delay(10L)
+        }
+        mVision.startListenFrame(StreamType.FISH_EYE
+        ) { streamType, frame -> mImgFishEye.copyPixelsFromBuffer(frame.byteBuffer) }
+    }
+
+    private suspend fun startDepthCamera() {
+        while (!mVision.isBind) {
+            delay(10L)
+        }
+        mVision.startListenFrame(StreamType.DEPTH
+        ) { streamType, frame -> mImgDepth.copyPixelsFromBuffer(frame.byteBuffer) }
+    }
 }
